aa50da9425e24f374e9621388e026fafcb2cb7c3
Chris Smith <chris@crumbdesign.co.uk>
2018-11-26 11:48:19 +0000

    Being doing a bit more work to the app

--- app/Environment Rescue/www/js/index.js
+++ app/Environment Rescue/www/js/index.js
@@ -3,11 +3,14 @@ function loadBackground(){
     width = $(window).width();
 }
 
+var currentPage = "home";
+
 function loadQuestions(button){
     var buttons = button.parentElement.getElementsByTagName("input");
     var area = button.dataset.name;
+    currentPage = area;
     for(i=buttons.length-1; i>-1; i--){
-        buttons[i].style.opacity = "0";
+        buttons[i].classList.add("hide");
     }
     setTimeout(function(){
         image.querySelector("#sky").classList.add(area);
@@ -20,5 +23,49 @@ function loadQuestions(button){
         image.querySelector("#sea").classList.add(area);
         image.querySelector("#trees").classList.add(area);
         image.querySelector("#ground").classList.add(area);
+        setTimeout(function(){
+            document.getElementById(area).classList.add("selected");
+        }, 800);
     },1100);
+}
+
+function returnHome(){
+    var buttons = document.getElementById("home").getElementsByTagName("input");
+    var area = currentPage;
+    document.getElementById(currentPage).classList.remove("selected");
+    setTimeout(function(){
+        image.querySelector("#sky").classList.remove(area);
+        image.querySelector("#clouds").classList.remove(area);
+        image.querySelector("#cloud2").classList.remove(area);
+        image.querySelector("#baloon").classList.remove(area);
+        image.querySelector("#mountains").classList.remove(area);
+        image.querySelector("#land").classList.remove(area);
+        image.querySelector("#Layer_1-2").classList.remove(area);
+        image.querySelector("#sea").classList.remove(area);
+        image.querySelector("#trees").classList.remove(area);
+        image.querySelector("#ground").classList.remove(area);
+        setTimeout(function(){
+            for(i=0; i<buttons.length; i++){
+                buttons[i].classList.remove("hide");
+            }
+        }, 700);
+    }, 700)
+}
+
+var questionNum;
+
+function nextQuestion(num){
+    questionNum = num;
+    if(num < 0){
+        num = 0
+    }
+    document.getElementById(currentPage).classList.remove("selected");
+    document.getElementById("questions").classList.remove("display");
+    document.getElementById("questions").classList.add("away");
+    setTimeout(function(){
+        document.getElementById("questions").classList.remove("away");
+        setTimeout(function(){
+            document.getElementById("questions").classList.add("display");
+        }, 50)
+    },700)
 }
