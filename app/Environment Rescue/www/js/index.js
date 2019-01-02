73bfb794f3f08f1d395186213a382878a1a0f69d
Chris Smith <chris@crumbdesign.co.uk>
2019-01-02 21:01:46 +0000

    I have started addiing the Login functionality to the app

--- app/Environment Rescue/www/js/index.js
+++ app/Environment Rescue/www/js/index.js
@@ -5,5 +5,35 @@ function loadPage(page){
     window.location = "Questions.html?" + page.dataset.name;
 }
 
+//Login information
+function changeMode(){
+    document.getElementById("login").classList.toggle("signup");
+}
 
+function signin(element){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText)
+        }
+    };
+    xhttp.open("POST", "http://netscapes-nodejs.crumbdesign.co.uk/signin", true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.send("email=" + element.getElementsByClassName("email")[0].value + "&password=" + element.getElementsByClassName("password")[0].value);
+}
 
+function signup(element){
+    if(element.getElementsByClassName("password")[0].value != element.getElementsByClassName("password")[1].value){
+        alert("Your passwords do not match!")
+    } else {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.responseText)
+            }
+        };
+        xhttp.open("POST", "http://netscapes-nodejs.crumbdesign.co.uk/signup", true);
+        xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhttp.send("email=" + element.getElementsByClassName("email")[0].value + "&password=" + element.getElementsByClassName("password")[0].value);
+    }
+}
