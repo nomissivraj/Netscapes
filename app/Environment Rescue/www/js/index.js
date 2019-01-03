8ae2218c70d95d8cc7fdfcddf7a55f15df3a6d99
Chris Smith <chris@crumbdesign.co.uk>
2019-01-03 22:47:55 +0000

    I have done a bit more to the login feature of the app

--- app/Environment Rescue/www/js/index.js
+++ app/Environment Rescue/www/js/index.js
@@ -1,6 +1,8 @@
 var questionNum;
 var questions;
 
+var userID;
+
 function loadPage(page){
     window.location = "Questions.html?" + page.dataset.name;
 }
@@ -14,10 +16,16 @@ function signin(element){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText)
+            var response = this.responseText.split("/");
+            if(response[0] == "success"){//If the login is successful this will get called.
+                userID = response[1];
+                closeLogin();
+            } else {
+                alert("Login invalid")
+            }
         }
     };
-    xhttp.open("POST", "http://netscapes-nodejs.crumbdesign.co.uk/signin", true);
+    xhttp.open("POST", "http://netscapes-nodejs.crumbdesign.co.uk/signinapp", true);
     xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhttp.send("email=" + element.getElementsByClassName("email")[0].value + "&password=" + element.getElementsByClassName("password")[0].value);
 }
@@ -29,11 +37,24 @@ function signup(element){
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                console.log(this.responseText)
+                var response = this.responseText.split("/");
+                if(response[0] == "success"){//If the login is successful this will get called.
+                    userID = response[1];
+                    closeLogin();
+                } else {
+                    alert("Login invalid")
+                }
             }
         };
-        xhttp.open("POST", "http://netscapes-nodejs.crumbdesign.co.uk/signup", true);
+        xhttp.open("POST", "http://netscapes-nodejs.crumbdesign.co.uk/signupapp", true);
         xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xhttp.send("email=" + element.getElementsByClassName("email")[0].value + "&password=" + element.getElementsByClassName("password")[0].value);
     }
+}
+
+function closeLogin(){
+    document.getElementById("login").classList.add("hide");
+    setTimeout(function(){
+        document.getElementById("home").classList.add("display");
+    },700);
 }
