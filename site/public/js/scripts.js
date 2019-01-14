eba8eb0eaed85b587981ea826b98516699c0f737
Simon <simonpeterjarvis@gmail.com>
2019-01-14 15:21:50 +0000

    updated function that handles animation stages to account for intro

--- site/public/js/scripts.js
+++ site/public/js/scripts.js
@@ -41,18 +41,25 @@ function isInView() {
         if (sections[i].getBoundingClientRect().top <= window.innerHeight * 0.30 && sections[i].getBoundingClientRect().top > -window.innerHeight / 1.4) {
             var currentSection = sections[i].classList[1];
             switch(currentSection) {
+                case 'section--intro':
+                    windowRatio();
+                    //console.log("section one");
+                    background.classList.add('stage--intro');
+                    background.classList.remove('stage--1', 'stage--2', 'stage--3', 'stage--4');
+                    frame.style.transform = "";
+                break;
                 case 'section--1':
                     windowRatio();
                     //console.log("section one");
                     background.classList.add('stage--1');
-                    background.classList.remove('stage--2', 'stage--3', 'stage--4');
+                    background.classList.remove('stage--intro', 'stage--2', 'stage--3', 'stage--4');
                     frame.style.transform = "";
                 break;
                 case 'section--2':
                     windowRatio(0.3);
                     //console.log("section two");
                     background.classList.add('stage--2');
-                    background.classList.remove('stage--1', 'stage--3', 'stage--4');
+                    background.classList.remove('stage--intro', 'stage--1', 'stage--3', 'stage--4');
                     //console.log(frame, ratioVal);
                     frame.style.transform = "translateY(-"+ratioVal+"px)";
                 break;
@@ -60,19 +67,19 @@ function isInView() {
                     windowRatio(0.41);
                     //console.log("section three");
                     background.classList.add('stage--3');
-                    background.classList.remove('stage--1', 'stage--2', 'stage--4');
+                    background.classList.remove('stage--intro', 'stage--1', 'stage--2', 'stage--4');
                     frame.style.transform = "translateY(-"+ratioVal+"px) translateX(-98vw)";
                 break;
                 case 'section--4':
                     windowRatio(0.9);
                     //console.log("section four");
                     background.classList.add('stage--4');
-                    background.classList.remove('stage--1', 'stage--2', 'stage--3');
+                    background.classList.remove('stage--intro', 'stage--1', 'stage--2', 'stage--3');
                     frame.style.transform = "translateY(-"+ratioVal+"px) translateX(-98vw)";
                 break;
                 default:
                 console.log("outside");
-                background.classList.remove('stage--1', 'stage--2', 'stage--3', 'stage--4');
+                background.classList.remove('stage--intro', 'stage--1', 'stage--2', 'stage--3', 'stage--4');
             }
             sections[i].classList.add('active');
         } else { sections[i].classList.remove('active')}
