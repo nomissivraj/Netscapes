030e896b2c0480c1189d0af490afb8488ed6d5c2
Simon <simonpeterjarvis@gmail.com>
2019-01-16 20:48:15 +0000

    Trying to fix scroll handling issues

--- site/public/js/scripts.js
+++ site/public/js/scripts.js
@@ -4,10 +4,54 @@ var sections,
     windowW,
     ratioVal;
 
+var pageScroll = true;
+
+function scrollAtBottom(el) {
+    if (Math.round(el.scrollTop) === (Math.round(el.scrollHeight - el.offsetHeight))) {
+        return true;
+    } else return false;
+}
+
+function scrollAtTop(el) {
+    if (Math.round(el.scrollTop === 0)) {
+        return true;
+    } else return false;
+}
+
+function disablePageScroll(obj, section) {
+    var mouseIn = obj.parentElement.classList[1];
+    if (window.innerWidth <= 1300 || window.innerHeight <= 800) {
+        if (mouseIn === currentSection) {       
+            pageScroll = false;
+
+        } else {
+            pageScroll = true;
+        }  
+    }
+}
+
+function enablePageScroll(obj, section) {
+    if (window.innerWidth <= 1300 || window.innerHeight <= 800) {
+        pageScroll = true;
+    }
+} 
+
+var sectionCont;
 window.onload = () => {
     sections = document.getElementsByClassName('section');
+    sectionCont = document.getElementsByClassName('section__content');
     background = document.getElementsByClassName('background')[0];
     frame = document.getElementsByClassName('background__frame')[0];
+    
+    for (let i = 0; i < sectionCont.length; i++) {
+        sectionCont[i].addEventListener('mouseover', () => {
+            disablePageScroll(sectionCont[i], sections[i]);
+        });
+        sectionCont[i].addEventListener('mouseout', () => {
+            enablePageScroll(sectionCont[i], sections[i]);
+        });
+    }
+
     isInView();
     loadJSON();
 }
@@ -34,13 +78,13 @@ function windowRatio(ratio) {
     console.log("heightOffset:" + heightOffset) */
 
 }
-
+var currentSection;
 function isInView() {
     if (!document.getElementsByClassName('section:not(.active)')) return;
-
+    if (!sections) return;
     for (var i = 0; i < sections.length; i++) {
         if (sections[i].getBoundingClientRect().top <= window.innerHeight * 0.30 && sections[i].getBoundingClientRect().top > -window.innerHeight / 1.4) {
-            var currentSection = sections[i].classList[1];
+            currentSection = sections[i].classList[1];
             switch(currentSection) {
                 case 'section--intro':
                     windowRatio();
@@ -142,6 +186,7 @@ function nextSection(direction) {
             if (index <= 0) return;
             index--;
             scrollEase(sections[index], direction);
+            scrollUpCount = 0;
             //sections[index].scrollIntoView();
             
         } else if (direction === "down") {
@@ -158,31 +203,41 @@ function nextSection(direction) {
 
 function inverse(num) {
     var result = num > 0 ? Math.abs(num) * -1 :  Math.abs(num) * + 1;
-    console.log("result", result);
+    //console.log("result", result);
     return result;
 }
 
+function scrollDirection(event) {
+     console.log(event.type);
+     if (event.type === "wheel") {
+        //console.log(event.wheelDeltaY || event);
+        var data = event.wheelDeltaY || inverse(event.deltaY);
+        if (data > 0) {
+            return "up";
+        } else return "down";
+    }
+}
 
 /* var scrollActive = false;//set scroll active to false by default */
 var scrollingActive;
 function scrolling(event) {
     if(canScroll){
-    //console.log(event.type);
-    if (event.type === "wheel") {
-        //console.log(event.wheelDeltaY || event);
-        var data = event.wheelDeltaY || inverse(event.deltaY);
-        data > 0 ? nextSection("up") : nextSection("down");
+        //console.log(event.type);
+        if (event.type === "wheel") {
+            //console.log(event.wheelDeltaY || event);
+            var data = event.wheelDeltaY || inverse(event.deltaY);
+            data > 0 ? nextSection("up") : nextSection("down");
 
-    } else if (event.type === "keyup") {
-       var input = event || window.event;
-       input.preventDefault();
-       if (input.keyCode == '38') {
-            nextSection("up")
-       } else if (input.keyCode == '40') {
-            nextSection("down");
-       } else return;
+        } else if (event.type === "keyup") {
+        var input = event || window.event;
+        input.preventDefault();
+        if (input.keyCode == '38') {
+                nextSection("up")
+        } else if (input.keyCode == '40') {
+                nextSection("down");
+        } else return;
+        }
     }
-}
 }
 
 
@@ -224,12 +279,39 @@ function resizePos() {
     }
 
 }
+var scrollUpCount = 0;
+function checkContScrollPos(event) {
+    var direction = scrollDirection(event);
+    console.log(direction)
+    if (direction === "up") {
+        scrollUpCount++;
+    }
+    console.log(scrollUpCount);
+    //get current section content element
+    for (let i = 0; i < sections.length; i++) {
+        if (sections[i].classList[1] === currentSection) {
+            if (scrollAtBottom(sectionCont[i]) && i !== sections.length - 1 && direction === "down") {//If inner content is scrolled to bottom and current section isn't last section
+                console.log("scroll at bottom")
+                pageScroll = true;
+                sectionCont[i].scrollTop = 0;
+            }
+            if (scrollAtTop(sectionCont[i]) && i !== 0 && scrollUpCount === 10) { //if inner content is scrolled to top and current section isn't first section
+                console.log("scroll at top")
+                pageScroll = true;
+            }
+        }
+    }
+    if (scrollUpCount > 10) scrollUpCount = 0;
+}
+
 
 //Self invoking anonymous function that monitors scrolling events and calls scrolling function to handle scroll direction and section navigation
 var scrollingTimeout;
 var sameScroll = false;
 (() => {
     document.onwheel = (e) => {
+        checkContScrollPos(e);
+        if (pageScroll === false) return;
         e.preventDefault(e)
         if(!sameScroll){
             scrolling(e);
